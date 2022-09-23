4d4614d02fa2d95c4962dba1cb42a85e625c6ee3
Lucas Raimundo <lucasraimundo2010@gmail.com>
2022-09-23 13:03:03 -0300

    Getting a post by id

---
+++ src/main/java/com/lucasraimundo/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.lucasraimundo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasraimundo.workshopmongo.domain.Post;
+import com.lucasraimundo.workshopmongo.repository.PostRepository;
+import com.lucasraimundo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
