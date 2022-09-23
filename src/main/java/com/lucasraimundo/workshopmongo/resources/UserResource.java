a02907245844e9a1d25c0a58446220ea342c1419
Lucas Raimundo <lucasraimundo2010@gmail.com>
2022-09-23 12:57:58 -0300

    Endpoint to return a user's posts

--- src/main/java/com/lucasraimundo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lucasraimundo/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lucasraimundo.workshopmongo.domain.Post;
 import com.lucasraimundo.workshopmongo.domain.User;
 import com.lucasraimundo.workshopmongo.dto.UserDTO;
 import com.lucasraimundo.workshopmongo.services.UserService;
@@ -65,4 +66,13 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
+	
 }
